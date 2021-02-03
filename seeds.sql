2777cf03de4d3571f9a00d35688dde63451090cc
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-03 18:23:15 -0500

    For the employee table, added 2 people as examples with the required information for each person. This includes first name, last name, role id number, and manager id number.

--- seeds.sql
+++ seeds.sql
@@ -6,3 +6,5 @@ VALUES ("HR"),("Sales"),("Payroll");
 INSERT INTO role (title, salary, department_id) 
 VALUES ("Salesperson", 40000, 1234);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ("John","Smith",678, 012), ("Samantha", "Jones", 901, 8652);
