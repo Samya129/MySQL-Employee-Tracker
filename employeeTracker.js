c782d930a362d5378191ceb93d92888d4af362bb
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-03 18:51:26 -0500

    Created a function that displays within the terminal a list of departments with their perspective ids.

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,11 +1,18 @@
-//Dependencies
-const connection = require("./connection")
-const cTable = require('console.table');
-const inquirer = require('inquirer');
+// Dependencies
+const connection = require("./connection");
+const cTable = require("console.table");
+const inquirer = require("inquirer");
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
-  connection.end();//close the connection
+  console.log("connected as id " + connection.threadId);
+  connection.end(); //close the connection
 });
 
+function getAllDepartments() {
+  connection.query("select * from department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+  });
+}
+getAllDepartments();
