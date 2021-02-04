c084fe5533a6a76645027c5f412287c22ba71c84
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-03 19:31:22 -0500

    Created a generalOptions of what a user could do question with 4 options, and created a promise and a switch statement for that question to return their perspective methods or functions that I have not created yet but appears in the terminal.

--- employeeTracker.js
+++ employeeTracker.js
@@ -12,24 +12,50 @@ connection.connect((err) => {
 function getAllDepartments() {
   connection.query("select * from department", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    //console.table(res);
   });
 }
 
 function getAllRoles() {
   connection.query("select * from role", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    //console.table(res);
   });
 }
 
 function getAllEmployees() {
   connection.query("select * from employee", function (err, res) {
     if (err) throw err;
-    console.table(res);
+    //console.table(res);
   });
 }
 
 getAllDepartments();
 getAllRoles();
 getAllEmployees();
+
+const generalOptions = () => {
+inquirer
+  .prompt([
+    {
+      name: "userChoice",
+      type: "list",
+      message: "Please select an option of what you would you like to do?",
+      choices: ["Add","View","Update", "Delete"],
+    },
+])
+.then((answer) => {
+switch (answer.userChoice){
+  case "Add":
+  return addWhat();
+  case "View":
+  return viewWhat();
+  case "Update":
+  return updateWhat();
+  case "Delete":
+    return deleteWhat();
+}
+//console.log(answer.userChoice)
+});
+};
+generalOptions();
