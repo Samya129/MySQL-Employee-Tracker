63618cb4cc04ba82c1b3c8d37ccd3a56235f8055
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-03 20:11:43 -0500

    Added a general addWhat function with the user to choose what type of addition they would like to do with their respective cases depending on the user's response. The return methods do not work at the moment yet because it is not written yet.

--- employeeTracker.js
+++ employeeTracker.js
@@ -34,28 +34,52 @@ getAllDepartments();
 getAllRoles();
 getAllEmployees();
 
+//General Questions:
 const generalOptions = () => {
-inquirer
-  .prompt([
-    {
-      name: "userChoice",
-      type: "list",
-      message: "Please select an option of what you would you like to do?",
-      choices: ["Add","View","Update", "Delete"],
-    },
-])
-.then((answer) => {
-switch (answer.userChoice){
-  case "Add":
-  return addWhat();
-  case "View":
-  return viewWhat();
-  case "Update":
-  return updateWhat();
-  case "Delete":
-    return deleteWhat();
-}
-//console.log(answer.userChoice)
-});
+  inquirer
+    .prompt([
+      {
+        name: "userChoice",
+        type: "list",
+        message: "Please select an option of what you would you like to do?",
+        choices: ["Add", "View", "Update", "Delete"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Add":
+          return addWhat();
+        case "View":
+          return viewWhat();
+        case "Update":
+          return updateWhat();
+        case "Delete":
+          return deleteWhat();
+      }
+      //console.log(answer.userChoice)
+    });
 };
 generalOptions();
+
+//Adding Information:
+const addWhat = () => {
+  inquirer
+    .prompt([
+      {
+        name: "adding",
+        type: "checkbox",
+        message: "What would you like to add?",
+        choices: ["Department", "Role", "Employee"],
+      },
+    ])
+    .then((response) => {
+      switch (response.userChoice) {
+        case "Department":
+          return addDepartment();
+        case "Role":
+          return addRole();
+        case "Employee":
+          return addEmployee();
+      }
+    });
+};
