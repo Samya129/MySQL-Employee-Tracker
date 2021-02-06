811e85b2ee02a8fa4eda43dcb538133b601dc439
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-05 21:18:48 -0500

    Added foreign keys for the ids within the employeeTracker.js file, and fixed number ids to start the files in order with the auot increment that I have currently.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ INSERT INTO department(name)
 VALUES ("HR"),("Sales"),("Payroll");
 
 INSERT INTO role (title, salary, department_id) 
-VALUES ("Salesperson", 40000, 1234);
+VALUES ("Salesperson", 40000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ("John","Smith",678, 012), ("Samantha", "Jones", 901, 8652);
+VALUES ("John","Smith",1, 1), ("Samantha", "Jones",3 , null);
