c4f65d62ddceadf678aa11e9eab8f17ff4b6b14f
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-06 20:48:53 -0500

    Fixed password issue.and files.

--- connection.js
+++ connection.js
@@ -1,21 +1,25 @@
 // Import the mysql package
-const mysql = require('mysql');
+const mysql = require("mysql");
+
+require("dotenv").config();
+
+//console.log(process.env.SECRETPASSWORD)
 
 // Connect to the employees_DB database using a localhost connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // Your port, if not 3306
-    port: 3306,
-  
-    // Your MySQL username
-    user: 'root',
-  
-    // Your MySQL password (leave blank for class demonstration purposes; fill in later)
-    password: '',
-  
-    // Name of database
-    database: 'employees_DB',
-  });
-  
-  module.exports = connection
+  host: "localhost",
+
+  // Your port, if not 3306
+  port: 3306,
+
+  // Your MySQL username
+  user: "root",
+
+  // Your MySQL password (leave blank for class demonstration purposes; fill in later)
+  password: process.env.SECRETPASSWORD,
+
+  // Name of database
+  database: "employees_DB",
+});
+
+module.exports = connection;
