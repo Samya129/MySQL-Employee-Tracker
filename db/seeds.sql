67e0d5d95cb6956df153bdb1c57ae3718505bd62
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-02-08 22:07:32 -0500

    Fixed correct manager to the set of employees and updated the seeds file that correspond to those changes.

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,4 +8,4 @@ VALUES ("Finance Manager", 50000, 1),("IT Manager", 65000,3),("Credit Analyst", 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ("Ron","Weasley", 1, null), ("Sirius", "Black", 2 , null),("Hermione", "Granger", 3 , 1), ("Neville", "Longbottom", 4 , null)
-,("Tom", "Jones", 5 , 3), ("Thomas", "Reed", 6 , null);
+,("Tom", "Jones", 5 , 2), ("Thomas", "Reed", 6 , null);
